d4f001de6abee58b017fc49a487ade2003d1d31d
AntonVasko <vasko.anton.a@gmail.com>
2026-02-02 21:34:23 +0300

    Update id_examples.py

--- lesson3/id_examples.py
+++ lesson3/id_examples.py
@@ -1,11 +1,9 @@
+# связано с файлом idexamples.kv
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import NumericProperty
 from kivy.uix.widget import Widget
 
 class MyFirstWidget(Widget):
     pass
-
 
 class IdExamplesApp(App):
     def build(self):
